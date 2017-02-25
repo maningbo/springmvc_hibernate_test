b9de90ed7cfb01acc364f4161de73f12a23445f6
马宁波 <maningbo0612@163.com>
2017-02-25 17:44:01 +0800

    修改注释

--- springmvc_hibernate_test/z_documents/mysql_create_tables.sql
+++ springmvc_hibernate_test/z_documents/mysql_create_tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nick_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '真实姓名',
+  `nick_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '昵称',
   `telephone` varchar(20) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '电话号码',
   `register_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '记录入库时间',
   PRIMARY KEY (`id`)
